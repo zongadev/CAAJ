46b282233d1250da9fe6282342a9c805873a78a4
Goncho <gonzimoyano@gmail.com>
2025-04-28 21:18:16 -0300

    trabajo sin los cambios del profe

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const suggestions = [
+    "Calculo Elementeal",
+    "Calculo Avanzado",
+    "Algebra y Geometria",
+    "Programacion Web y Movil",
+    "Informatica General",
+    "Fisica I",
+  ];
+  const searchInput = document.getElementById("search");
+  const suggestionsBox = document.getElementById("suggestions");
+
+  searchInput.addEventListener("input", () => {
+    const query = searchInput.value.toLowerCase();
+    suggestionsBox.innerHTML = "";
+
+    if (!query) {
+      suggestionsBox.style.display = "none";
+      return;
+    }
+
+    const filtered = suggestions.filter((item) =>
+      item.toLowerCase().includes(query)
+    );
+
+    if (filtered.length) {
+      filtered.forEach((item) => {
+        const div = document.createElement("div");
+        div.textContent = item;
+        div.addEventListener("click", () => {
+          searchInput.value = item;
+          suggestionsBox.style.display = "none";
+        });
+        suggestionsBox.appendChild(div);
+      });
+      suggestionsBox.style.display = "flex";
+    } else {
+      suggestionsBox.style.display = "none";
+    }
+  });
+});
