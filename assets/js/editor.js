fb4f093c0a902d07d5e4df1218638f1828e01d1b
otto-f32 <lucasherrera@uca.edu.ar>
2025-05-19 13:34:14 -0300

    Validación editor
    JS para validacion y cambios minimos (required) en HTML

---
+++ assets/js/editor.js
@@ -0,0 +1,29 @@
+function customValidez(campo, mensaje) { // Cambia el mensaje de error
+  campo.setCustomValidity(mensaje);
+}
+
+function check(ID) { // Chequea el campo
+  campo.setCustomValidity("");
+
+  if (campo.validity.valueMissing) { // Si el campo está vacío
+    customValidez(campo, "Este campo debe de estar completo");
+  } else if (campo.validity.patternMismatch) { // Si el formato es incorrecto
+    customValidez(campo, "El formato es incorrecto");
+  } else {
+    campo.setCustomValidity(""); // Si el campo es válido, no se establece un mensaje de error
+  }
+  campo.reportValidity(); // Muestra el mensaje de error si es necesario
+}
+
+document.querySelector('.editor-form').addEventListener('submit', function (e) { // Al enviar el formulario
+    let campos = ['titulo', 'contenido']; // lista de IDs de los campos a validar
+    let valido = true;
+    for (var i = 0; i < campos.length; i++) { // Recorre los campos
+      var id = campos[i];
+      var campo = document.getElementById(id); // Obtiene el campo por ID
+      check(id);
+      if (!campo.checkValidity()) { // Si el campo no es false, se bolquea el envio
+        valido = false;
+      }
+    }
+  });
