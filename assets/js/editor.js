c943a0c043e8d78fe48934cd99c0b97d1c6693d7
molocodev <8769951+molocodev@users.noreply.github.com>
2025-05-23 10:07:04 -0300

    Mejora comentarios

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -89,7 +89,7 @@ document.querySelector("#submit-btn").addEventListener("click", function (e) {
   }
 });
 
-// Real-time validation (input/change)
+// Validar cambios en los inputs en tiempo real
 ["titulo", "materia", "contenido", "tags", "archivo", "visibilidad"].forEach(function (id) {
   var campo = document.getElementById(id);
   if (campo) {
